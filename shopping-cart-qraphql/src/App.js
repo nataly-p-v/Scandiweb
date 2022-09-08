494ae2743f732e6acc7de4fa3a28166529b32574
nataly_p_v@ukr.net <nataly_p_v@ukr.net>
2022-09-08 12:38:21 +0200

    third commit

--- shopping-cart-qraphql/src/App.js
+++ shopping-cart-qraphql/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import AllProducts from './AllProducts';
+import Product from './Product';
 
-function App() {
+export default () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        asd
       </header>
     </div>
   );
 }
-
-export default App;
