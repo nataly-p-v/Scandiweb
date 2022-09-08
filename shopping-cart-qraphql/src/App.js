1999b2d2d8426c49c27216012824422bfccd0a1c
nataly_p_v@ukr.net <nataly_p_v@ukr.net>
2022-09-08 12:50:06 +0200

    5 commit

---
+++ shopping-cart-qraphql/src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import AllProducts from './AllProducts';
+import Product1 from './Product1.js';
+
+export default () => {
+const [selectedProductId, setSelectedProductId] = useState();
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        asd
+      </header>
+<Product1 />
+    </div>
+  );
+}
+
