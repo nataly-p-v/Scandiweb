337ae5b8f4a1b50798f7b7d2b13a75fb9b80a047
nataly_p_v@ukr.net <nataly_p_v@ukr.net>
2022-09-19 15:41:42 +0200

    added new component for cartitems state update

--- shopping-cart/src/components/Cart.jsx
+++ shopping-cart/src/components/Cart.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import AttributeCart from "./AttributeCart";
+import CartItem from "./CartItem";
 
-function Cart({cartItems, removeFromCart, selectedOption, handleDecreaseCart, handleIncreaseCart, attributeMap}) {
+function Cart({cartItems, selectedOption, attributeMap}) {
       const [selectedAttrId, setSelectedAttrId] = useState(null);
       let [itemQuantity, setItemQuantity] = useState(1);
               useEffect(() => {
@@ -16,38 +16,7 @@ function Cart({cartItems, removeFromCart, selectedOption, handleDecreaseCart, ha
             <ul className="main-cart">
                 <div className="main-cart__items">
                      {cartItems.length>0 ? cartItems.map((item, i) => (
-                        <li className="main-cart__item" key={Math.random()}>
-                        <div>
-                            <h2 className="main-cart__items-name">{item.name}</h2>
-                            <h3>{item.brand}</h3>
-                             <ul className="product-card__prices">
-                                {(item.prices.map((price,i) => {
-                                     return (price.currency.symbol === selectedOption) ? <li className="prices__item" key={Math.random()}>
-                                     {price.currency.symbol}{price.amount}  </li> : '';
-                                  }))}
-                             </ul>
-                              <div className="product-card__attributes">
-                                 {(item.attributes.map((attr,i) => {
-                                       return <div key={Math.random()} className="product-card__attributes-item">
-                                                <span className="attributes__item-name">{attr.name}</span>
-                                                <div className="attributes__item-values">
-                                                    <AttributeCart
-                                                    attr={attr} attributeMap={attributeMap} attrName={attr.name} selectedAttrId={selectedAttrId} />
-                                                 </div>
-                                       </div> ;
-                                    }))}
-                              </div>
-                        </div>
-                             <div className="cart-product-quantity">
-                                 <button className="cart-product-plus"onClick={() => {setItemQuantity(itemQuantity+1)}}>+</button>
-                                 <div className="count">{itemQuantity}</div>
-                                 <button
-                                 className={`cart-product-minus ${(itemQuantity<=1)  ? ' v-hidden' : 'v-visible'}`}
-                                 onClick={() => setItemQuantity(itemQuantity-1)}>-</button>
-                             </div>
-                             <div className="product-card__img"><img src={item.gallery[0]} alt="product"></img></div>
-                             <button className="product-card__remove" onClick={() => removeFromCart(item)}>X</button>
-                        </li>
+                        <CartItem item={item} cartItems={cartItems} selectedOption={selectedOption} attributeMap={attributeMap}/>
                      ))
                      : <h1 className="cart-name">0 items added to cart!</h1>
                      }
---
+++ shopping-cart/src/components/CartItem.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import AttributeCart from "./AttributeCart";
+
+function CartItem({item, cartItems, selectedOption, attributeMap}) {
+      const [selectedAttrId, setSelectedAttrId] = useState(null);
+      let [itemQuantity, setItemQuantity] = useState(1);
+              useEffect(() => {
+              attributeMap.forEach((i) => {
+                  setSelectedAttrId(i);
+              })
+              console.log(itemQuantity)
+              }, []);
+        return (
+        <div>
+            <li className="main-cart__item" key={Math.random()}>
+            <div>
+                <h2 className="main-cart__items-name">{item.name}</h2>
+                <h3>{item.brand}</h3>
+                 <ul className="product-card__prices">
+                    {(item.prices.map((price,i) => {
+                         return (price.currency.symbol === selectedOption) ? <li className="prices__item" key={Math.random()}>
+                         {price.currency.symbol}{price.amount}  </li> : '';
+                      }))}
+                 </ul>
+                  <div className="product-card__attributes">
+                     {(item.attributes.map((attr,i) => {
+                           return <div key={Math.random()} className="product-card__attributes-item">
+                                    <span className="attributes__item-name">{attr.name}</span>
+                                    <div className="attributes__item-values">
+                                        <AttributeCart
+                                        attr={attr} attributeMap={attributeMap} attrName={attr.name} selectedAttrId={selectedAttrId} />
+                                     </div>
+                           </div> ;
+                        }))}
+                  </div>
+            </div>
+                 <div className="cart-product-quantity">
+                     <button className="cart-product-plus"onClick={() => {setItemQuantity(itemQuantity+1)}}>+</button>
+                     <div className="count">{itemQuantity}</div>
+                     <button
+                     className={`cart-product-minus ${(itemQuantity<=1)  ? ' v-hidden' : 'v-visible'}`}
+                     onClick={() => setItemQuantity(itemQuantity-1)}>-</button>
+                 </div>
+                 <div className="product-card__img"><img src={item.gallery[0]} alt="product"></img></div>
+                 <button className="product-card__remove" onClick={() => console.log(item)}>X</button>
+            </li>
+        </div>
+        )
+}
+export default CartItem;
